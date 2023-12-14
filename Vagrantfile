6c2b2e50d27472cbe5244956a651483ecfca6e5b
FeeLinS9 <feelins@mail.ru>
2023-12-14 17:59:20 +0300

    Package Management

--- Vagrantfile
+++ Vagrantfile
@@ -1,15 +1,28 @@
 # -*- mode: ruby -*- 
 # vi: set ft=ruby : vsa
-Vagrant.configure(2) do |config| 
-  config.vm.box = "generic/centos8s" 
-  config.vm.box_version = "4.3.4" 
-  config.vm.provider "virtualbox" do |v| 
-  v.memory = 512 
-  v.cpus = 1 
-  config.vm.network "private_network", ip: "192.168.56.10"
- # config.vm.network "forwarded_port", guest: 80, host: 80
-  config.vm.hostname = "pm" 
- # config.vm.provision "shell", path: "nfss_script.sh"
-  end 
- end 
- 
+MACHINES = {
+  :"pm" => {
+              :box_name => "generic/centos8s",
+              :box_version => "4.3.4",
+              :cpus => 1,
+              :memory => 1024,
+              :ip_addr => '192.168.56.10',
+            }
+}
+
+Vagrant.configure("2") do |config|
+  MACHINES.each do |boxname, boxconfig|
+    config.vm.synced_folder ".", "/vagrant", disabled: true
+    config.vm.define boxname do |box|
+      box.vm.box = boxconfig[:box_name]
+      box.vm.box_version = boxconfig[:box_version]
+      box.vm.host_name = boxname.to_s
+      box.vm.network "private_network", ip: boxconfig[:ip_addr]
+      box.vm.provider "virtualbox" do |v|
+        v.memory = boxconfig[:memory]
+        v.cpus = boxconfig[:cpus]
+      box.vm.provision "shell", path: "config.sh"
+      end
+    end
+  end
+end
--- config.sh
+++ config.sh
@@ -1,3 +1,5 @@
+sudo -i
+cd /root
 yum install -y \
 redhat-lsb-core \
 wget \
@@ -10,14 +12,26 @@ wget https://nginx.org/packages/centos/8/SRPMS/nginx-1.20.2-1.el8.ngx.src.rpm
 rpm -i nginx-1.*
 wget https://github.com/openssl/openssl/archive/refs/heads/OpenSSL_1_1_1-stable.zip
 unzip OpenSSL_1_1_1-stable.zip
-mv OpenSSL_1_1_1-stable openssl
-yum-builddep rpmbuild/SPECS/nginx.spec -y
-wget -O rpmbuild/SPECS/nginx.spec https://raw.githubusercontent.com/FeeLinS9/lesson7/master/nginx.spec
-rpmbuild -bb rpmbuild/SPECS/nginx.spec
-yum localinstall -y \rpmbuild/RPMS/x86_64/nginx-1.20.2-1.el8.ngx.x86_64.rpm
+mv openssl-OpenSSL_1_1_1-stable /root/openssl
+yum-builddep /root/rpmbuild/SPECS/nginx.spec -y
+wget -O /root/rpmbuild/SPECS/nginx.spec https://raw.githubusercontent.com/FeeLinS9/lesson7/master/nginx.spec
+rpmbuild -bb /root/rpmbuild/SPECS/nginx.spec
+yum localinstall -y \/root/rpmbuild/RPMS/x86_64/nginx-1.20.2-1.el8.ngx.x86_64.rpm
 systemctl start nginx
 mkdir /usr/share/nginx/html/repo
-cp rpmbuild/RPMS/x86_64/nginx-1.20.2-1.el8.ngx.x86_64.rpm /usr/share/nginx/html/repo
-wget https://downloads.percona.com/downloads/percona-distribution-mysql-ps/percona-distribution-mysql-ps-8.0.28/binary/redhat/8/x86_64/percona-orchestrator-3.2.6-2.el8.x86_64.rpm -O /usr/share/nginx/html/repo/percona-orchestrator-3.2.6-2.el8.x86_64.rpm
+cp /root/rpmbuild/RPMS/x86_64/nginx-1.20.2-1.el8.ngx.x86_64.rpm /usr/share/nginx/html/repo
+wget https://downloads.percona.com/downloads/percona-distribution-mysql-ps/percona-distribution-mysql-ps-8.1.0/binary/redhat/8/x86_64/percona-orchestrator-3.2.6-11.el8.x86_64.rpm -O /usr/share/nginx/html/repo/percona-orchestrator-3.2.6-11.el8.x86_64.rpm
 createrepo /usr/share/nginx/html/repo/
 wget -O /etc/nginx/conf.d/default.conf https://raw.githubusercontent.com/FeeLinS9/lesson7/master/default.conf
+nginx -s reload
+cat >> /etc/yum.repos.d/otus.repo << EOF
+[otus]
+name=otus-linux
+baseurl=http://localhost/repo
+gpgcheck=0
+enabled=1
+EOF
+yum repolist enabled | grep otus
+echo "Устанавливаем percona orchestrator из своего репозитория"
+yum install percona-orchestrator.x86_64 -y
+exit
